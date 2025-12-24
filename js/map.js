ad0683476544c3969738f9660be0d05c2fc9ae9f
QWEL.DESIGN <webmaster@qwel.design>
2025-12-24 15:27:30 +0900

    Register the properties

--- js/map.js
+++ js/map.js
@@ -7,27 +7,69 @@ export default class Map {
       zoomControl: true,
       scrollWheelZoom: false,
       doubleClickZoom: false
-    }).setView([36.094336, 136.032054], 17); // 国見公民館を中心に設定
+    }).setView([36.094336, 136.032054], 15); // 国見公民館を中心に設定
+
+    // ダブルクリックでズームアウト
+    this.map.on('dblclick', () => {
+      this.map.setZoom(15, { animate: true });
+    });
 
     // タイルレイヤーの追加 (OpenStreetMap)
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; OpenStreetMap contributors'
     }).addTo(this.map);
 
+    // アイコンの定義
+    this.icons = {
+      "調査前": L.icon({
+        iconUrl: "./assets/pin-gray.png",
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34]
+      }),
+      "調査済": L.icon({
+        iconUrl: "./assets/pin-blue.png",
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34]
+      }),
+      "案内可": L.icon({
+        iconUrl: "./assets/pin-green.png",
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34]
+      }),
+      "拠点": L.icon({
+        iconUrl: "./assets/pin-yellow.png",
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34]
+      }),
+      "施設": L.icon({
+        iconUrl: "./assets/pin-red.png",
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34]
+      })
+    };
+
+
     // 物件データ読み込み
     fetch('/properties/properties.json')
       .then(res => res.json())
       .then(properties => {
         properties.forEach(p => {
-          const marker = L.marker([p.lat, p.lng]).addTo(this.map);
+          const icon = this.icons[p.category] ?? this.icons["調査前"];
+          const marker = L.marker([p.lat, p.lng], { icon }).addTo(this.map);
 
           // ポップアップ設定
           marker.bindPopup(`
-            <strong>${p.name}</strong>
-            <br><a href="${p.url}" target="_blank">詳細を見る</a>
+            <strong>${p.name} | ${p.category}</strong>
+            <br><span>${p.address}</span>
+            <br><a href="#${p.id}">詳細を見る</a>
           `);
 
-          // クリックで移動
+          // クリックで移動・ズーム
           marker.on('click', () => {
             this.map.setView(marker.getLatLng(), 17);
           });
